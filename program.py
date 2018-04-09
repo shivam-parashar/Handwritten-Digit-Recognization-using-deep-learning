c91b0c108ce4b0aafca200029da8bdbd44af1017
shivam-parashar <mailtoageek@gmail.com>
2018-04-10 00:05:50 +0530

    Importing required packages

---
+++ program.py
@@ -0,0 +1,13 @@
+# Larger CNN for the MNIST Dataset
+import numpy
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
