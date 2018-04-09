26a366fd46f8dcdb0639bab695d3653e588a9dfb
shivam-parashar <mailtoageek@gmail.com>
2018-04-10 00:15:52 +0530

    Loading the MNIST dataset in Keras

--- program.py
+++ program.py
@@ -11,3 +11,16 @@ from keras.utils import np_utils
 from keras import backend as K
 K.set_image_dim_ordering('th')
 
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+
+# reshape to be [samples][pixels][width][height]
+X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
