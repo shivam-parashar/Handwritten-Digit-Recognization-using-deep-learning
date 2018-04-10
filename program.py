f2fda9098f2f7d1b9d5feb5a4c2d85b3d7da5ec7
shivam-parashar <mailtoageek@gmail.com>
2018-04-10 18:32:17 +0530

    model is fitting over 10 epochs with a batch size of 200

--- program.py
+++ program.py
@@ -53,3 +53,17 @@ def larger_model():
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
+	
+	
+	
+# build the model
+model = larger_model()
+
+
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
+
+
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
